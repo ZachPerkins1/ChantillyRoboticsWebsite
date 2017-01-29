70f6cda7b912a773a7040930db22cf0147b08e75
kvverti <1050cn@gmail.com>
2017-01-29 11:24:39 -0500

    Add header and footer files.
    Most browsers don't support loading from local files, so the header and footer won't appear if you open the page. It should work in a server environment though.

--- js/script.js
+++ js/script.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
       $(this).next().slideToggle(300); //Goes inside the contentbox, finds all p's, get's the second (index 1)
     });
   }
+  //Note: The load functions will not load local files in Chrome, Internet Explorer, or Edge.
+  //They should work when the files are remote hosted though.
+  //If you want to see the header and footer loaded on local, use Firefox.
+  $("#header").load("header.html #header-body");
+  $("#footer").load("footer.html #footer-body");
 });
