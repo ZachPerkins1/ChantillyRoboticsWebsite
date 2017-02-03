373eee05fcfbda779c54600866d91e47bf46d472
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-02-03 13:57:46 -0500

    Hide about content on page load

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   if($("#about-first").length!=0){ //So it only runs on the one page
+    //Hide all subtitle contents at first
+    $(".content-box").find("p").next().hide();
+    //show contents when headers are clicked
     $(".subtitle").click(function(){
       $(this).next().slideToggle(300); //Goes inside the contentbox, finds all p's, get's the second (index 1)
     });
