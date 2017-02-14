fea96abebf86f7a783cf029b5bff7ef7aa31c87e
Chris Nero <chrisvn00@gmail.com>
2017-02-13 19:01:16 -0500

    Add FIRST more info description

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  if($("#about-first").length!=0){ //So it only runs on the one page
+  /*if($("#about-first").length!=0){ //So it only runs on the one page
     //Hide all subtitle contents at first
     $(".content-box").find("p").next().hide();
     //show contents when headers are clicked
     $(".subtitle").click(function(){
       $(this).next().slideToggle(300); //Goes inside the contentbox, finds all p's, get's the second (index 1)
     });
-  }
+  }*/
   //Note: The load functions will not load local files in Chrome, Internet Explorer, or Edge.
   //They should work when the files are remote hosted though.
   //If you want to see the header and footer loaded on local, use Firefox.
