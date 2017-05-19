a44a7128f632f8c1741771c2899238ed8044c4a9
Zach Perkins <thezperk@gmail.com>
2017-05-19 16:54:48 +0000

    Got rid of comments

--- static/js/admin/list-loader.js
+++ static/js/admin/list-loader.js
@@ -202,17 +202,14 @@ var listLoader = {
 function clone(obj) {
     var copy;
  
-    // Handle the 3 simple types, and null or undefined
     if (null == obj || "object" != typeof obj) return obj;
  
-    // Handle Date
     if (obj instanceof Date) {
         copy = new Date();
         copy.setTime(obj.getTime());
         return copy;
     }
  
-    // Handle Array
     if (obj instanceof Array) {
         copy = [];
         for (var i = 0, len = obj.length; i < len; i++) {
@@ -221,7 +218,6 @@ function clone(obj) {
         return copy;
     }
  
-    // Handle Object
     if (obj instanceof Object) {
         copy = {};
         for (var attr in obj) {
